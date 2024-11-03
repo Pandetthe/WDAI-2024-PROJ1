90921f139d383cae9c8c530855ff650835f0c1cb
Jakub Jurczyk <jjurczyk@student.agh.edu.pl>
2024-11-03 17:09:07 +0100

    Add preview of portfolio images

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -11,5 +11,22 @@ const initApp = () => {
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
 }
+function openModal(imageUrl) {
+    const modal = document.getElementById('imageModal');
+    const modalImage = document.getElementById('modalImage');
+    // Preloading
+    const img = new Image();
+    img.onload = () => {
+        modalImage.src = imageUrl;
+        modal.classList.remove('hidden');
+        modal.classList.add('flex');
+    }
+    img.src = imageUrl;
+}
+function closeModal() {
+    const modal = document.getElementById('imageModal');
+    modal.classList.add('hidden');
+    modal.classList.remove('flex');
+}
 
 document.addEventListener('DOMContentLoaded', initApp)
