1889d64b850899062f17e78538ee4aa89aa26e4a
Jakub Jurczyk <jjurczyk@student.agh.edu.pl>
2024-11-04 10:18:48 +0100

    Add offers animation

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -29,4 +29,52 @@ function closeModal() {
     modal.classList.remove('flex');
 }
 
+function moveCards(direction) {
+    const leftCard = document.getElementById('left-card');
+    const middleCard = document.getElementById('middle-card');
+    const rightCard = document.getElementById('right-card');
+    if (direction === 'left') {
+        leftCard.classList.remove('z-[2]');
+        rightCard.classList.add('animate-right-middle', 'z-[2]');
+        middleCard.classList.add('animate-middle-left', 'z-[2]');
+        leftCard.classList.add('animate-left-right');
+        setTimeout(() => {
+            rightCard.classList.remove('animate-right-middle');
+            middleCard.classList.remove('animate-middle-left');
+            leftCard.classList.remove('animate-left-right');
+            let innerHTML = leftCard.innerHTML;
+            let classList = leftCard.classList.value;
+            leftCard.innerHTML = middleCard.innerHTML;
+            leftCard.className = middleCard.className;
+            middleCard.innerHTML = rightCard.innerHTML;
+            middleCard.className = rightCard.className;
+            rightCard.innerHTML = innerHTML;
+            rightCard.className = classList;
+            middleCard.classList.add('scale-125');
+            leftCard.classList.remove('scale-125');
+        }, 500);
+    }
+    else if (direction === 'right') {
+        rightCard.classList.remove('z-[2]');
+        rightCard.classList.add('animate-right-left');
+        middleCard.classList.add('animate-middle-right', 'z-[2]');
+        leftCard.classList.add('animate-left-middle', 'z-[2]');
+        setTimeout(() => {
+            rightCard.classList.remove('animate-right-left');
+            middleCard.classList.remove('animate-middle-right');
+            leftCard.classList.remove('animate-left-middle');
+            let innerHTML = leftCard.innerHTML;
+            let classList = leftCard.classList.value;
+            leftCard.innerHTML = rightCard.innerHTML;
+            leftCard.className = rightCard.className;
+            rightCard.innerHTML = middleCard.innerHTML;
+            rightCard.className = middleCard.className;
+            middleCard.innerHTML = innerHTML;
+            middleCard.className = classList;
+            middleCard.classList.add('scale-125');
+            rightCard.classList.remove('scale-125');
+        }, 500);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', initApp)
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,13 @@ module.exports = {
       animation: {
         'bg-move-dark': 'bg-move-dark 15s linear infinite',
         'bg-move-light': 'bg-move-light 15s linear infinite',
-        'open-menu': 'open-menu 0.5s ease-in-out forwards', 
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+        'middle-right': 'middle-right 0.5s ease-in-out forwards', 
+        'left-middle': 'left-middle 0.5s ease-in-out forwards',
+        'right-left': 'right-left 0.5s ease-in-out forwards',
+        'right-middle': 'right-middle 0.5s ease-in-out forwards', 
+        'middle-left': 'middle-left 0.5s ease-in-out forwards',
+        'left-right': 'left-right 0.5s ease-in-out forwards',
       },
       keyframes: {
         'bg-move-dark': {
@@ -35,6 +41,32 @@ module.exports = {
           '0%': { transform: 'scaleY(0)' },
           '80%': { transform: 'scaleY(1.2)' },
           '100%': { transform: 'scaleY(1)' },
+        },
+        'middle-right': {
+          '0%': { transform: 'translate(0, 0) scale(1.25)' },
+          '100%': { transform: 'translate(calc(8.9rem + 100% + 36px), 0) scale(1)', transformOrigin: 'left' },
+        },
+        'middle-left': {
+          '0%': { transform: 'translate(0, 0) scale(1.25)' },
+          '100%': { transform: 'translate(calc(-8.9rem - 100% - 36px), 0) scale(1)', transformOrigin: 'left' },
+        },
+        'left-right': {
+          '0%': { transform: 'translate(0, 0) scale(1)' },
+          '50%': { transform: 'translate(calc(8.9rem + 100% + 36px), 0) scale(0.75)' },
+          '100%': { transform: 'translate(calc(8.9rem * 2 + 200% + 72px), 0) scale(1)' },
+        },
+        'right-left': {
+          '0%': { transform: 'translate(0, 0) scale(1)' },
+          '50%': { transform: 'translate(calc(-8.9rem - 100% - 36px), 0) scale(0.75)' },
+          '100%': { transform: 'translate(calc(-8.9rem * 2 - 200% - 72px), 0) scale(1)' },
+        },
+        'left-middle': {
+          '0%': { transform: 'translate(0, 0) scale(1)', transformOrigin: 'left' },
+          '100%': { transform: 'translate(calc(8.9rem + 100% + 36px), 0) scale(1.25)' },
+        },
+        'right-middle': {
+          '0%': { transform: 'translate(0, 0) scale(1)', transformOrigin: 'left' },
+          '100%': { transform: 'translate(calc(-8.9rem - 100% - 36px), 0) scale(1.25)' },
         },
       }
     },
